5d9361f3a056fd9e392c6f05bb766826e31ec97b
carobte <caro.bustamante.escobar@gmail.com>
2024-10-25 08:55:41 -0500

    feat: Event index

--- resources/views/layouts/personal.blade.php
+++ resources/views/layouts/personal.blade.php
@@ -29,7 +29,7 @@
                 <a href="/" class="flex items-center">
                     <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9"
                         alt="JaviMarket Logo" />
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">JaviMarket</span>
+                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">CordiSimple</span>
                 </a>
 
                 <div class="flex items-center lg:order-2">
@@ -78,13 +78,13 @@
                         </li>
                         <li>
                             <a href="{{ route('events.index') }}"
-                                class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">events</a>
+                                class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Events</a>
                         </li>
-                        <li>
+                        {{-- <li>
                             <a href="#"
                                 class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Products</a>
                         </li>
-                        {{-- <li>
+                         <li>
                             <a href="#"
                                 class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features_</a>
                         </li>
