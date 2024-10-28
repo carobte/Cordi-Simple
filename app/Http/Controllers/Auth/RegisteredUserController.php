efc25865e53ae93a71dea5c5d65cc0ab575bf867
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2024-10-28 13:35:14 -0500

    feat: role is implemented when creating a user

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,6 +39,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'rols_id' => 2, // Default role: General User
         ]);
 
         event(new Registered($user));
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,9 +1,8 @@
-<section>
+<section class="max-w-7xl mx-auto p-6 bg-white dark:bg-gray-800 rounded-lg shadow-md">
     <header>
         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Profile Information') }}
         </h2>
-
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __("Update your account's profile information and email address.") }}
         </p>
@@ -13,9 +12,16 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" id="user-edit-form">
         @csrf
         @method('patch')
+
+        <div>
+            <x-input-label for="rols_id" :value="__('Role del Usuario')" />
+            <div class="mt-1 block w-full bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 p-2 rounded-md">
+                {{ $user->rol->name}}
+            </div>
+        </div>
 
         <div>
             <x-input-label for="name" :value="__('Name')" />
@@ -29,10 +35,9 @@
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
 
             @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
+                <div class="mt-2">
+                    <p class="text-sm text-gray-800 dark:text-gray-200">
                         {{ __('Your email address is unverified.') }}
-
                         <button form="send-verification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
                             {{ __('Click here to re-send the verification email.') }}
                         </button>
@@ -47,18 +52,38 @@
             @endif
         </div>
 
-        <div class="flex items-center gap-4">
+        <div class="flex items-center justify-between">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm text-gray-600 dark:text-gray-400">
+                    {{ __('Saved.') }}
+                </p>
             @endif
         </div>
     </form>
 </section>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        document.getElementById('user-edit-form').addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            Swal.fire({
+                title: "¿Quieres actualizar tu informacion?",
+                showDenyButton: true,
+                showCancelButton: false,
+                confirmButtonText: "Actualizar",
+                denyButtonText: `No Actualizar`
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire("informacion actualizado con éxito!", "", "success").then(() => {
+                        this.submit();
+                    });
+                } else if (result.isDenied) {
+                    Swal.fire("Se canceló la actualización", "", "info");
+                }
+            });
+        });
+    });
+</script>
