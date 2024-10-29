c78912503eeb886a5a353a2dbfa36e783e210ba1
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2024-10-29 11:24:19 -0500

    feat: create controller and request of reservation

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,8 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ReservationRequest;
+use App\Models\Reservation;
 
 class ReservationController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('reservations.create');
+    }
+
+    public function store(ReservationRequest $request)
+    {
+        $validatedData = $request->validated();
+        Reservation::create($validatedData);
+        return redirect()->route('reservations.index')->with('success', 'Reservacion creado exitosamente.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
 }
