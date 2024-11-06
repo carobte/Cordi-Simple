41db17729be9777fce3db56c13ef75651d0190f6
carobte <caro.bustamante.escobar@gmail.com>
2024-11-06 10:55:01 -0500

    Fix: Reservation admin view

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -2,7 +2,11 @@
 
 @section('content')
     <div class="container mx-auto py-8">
-        <h1 class="text-3xl font-bold text-center text-gray-800 mb-6">Mis Reservas</h1>
+        @if (Auth::user()->rol->name == 'administrator')
+            <h1 class="text-3xl font-bold text-center text-gray-800 mb-6">Reservas</h1>
+        @else
+            <h1 class="text-3xl font-bold text-center text-gray-800 mb-6">Mis Reservas</h1>
+        @endif
 
         {{-- Check if there are no reservations for the user --}}
         @if ($reservations->isEmpty())
@@ -11,50 +15,86 @@
             {{-- Display each reservation in a card layout --}}
             <div class="flex justify-center items-center flex-wrap gap-5 overflow-x-auto max-w-7xl mx-auto">
                 @foreach ($reservations as $reservation)
-                    <div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96 hover:border-slate-300 hover:shadow-md transition-all cursor-pointer">
+                    <div
+                        class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96 hover:border-slate-300 hover:shadow-md transition-all cursor-pointer">
                         <div class="p-4">
 
-                            {{-- Event details --}}
-                            <p class="text-slate-800 text-lg font-semibold capitalize"><strong>Nombre:</strong> {{ $reservation->event->name }}</p>
-                            <p class="text-slate-600 leading-normal font-light my-3"><strong>Descripción:</strong> {{ ucfirst($reservation->event->description) }}</p>
-                            <p class="text-slate-600 leading-normal font-light my-3 capitalize"><strong>Ubicación:</strong> {{ $reservation->event->location }}</p>
-                            <p class="text-slate-600 leading-normal font-light my-3"><strong>Fecha de Inicio:</strong> {{ $reservation->event->date_start }}</p>
-                            <p class="text-slate-600 leading-normal font-light my-3"><strong>Fecha de Finalización:</strong> {{ $reservation->event->date_end }}</p>
-
-                            <p class="{{ $reservation->status == 0 ? 'text-red-500' : 'text-green-500' }} leading-normal font-light my-3">
-                                <strong>Estado:</strong> {{ $reservation->status == 0 ? 'Cancelado' : 'Activo' }}
+                            {{-- Reserve details --}}
+                            <p class="leading-normal text-slate-800 text-lg font-semibold"><strong>ID de la
+                                    Reserva:</strong>
+                                {{ $reservation->id }}</p>
+                            <p class="leading-normal text-slate-800 text-lg font-semibold"><strong>Evento:</strong>
+                                <span class="text-slate-600 font-light my-3 capitalize">
+                                    {{ $reservation->event->name }}
+                                </span>
                             </p>
+                            @if (Auth::user()->rol->name == 'administrator')
 
-                            <div class="flex justify-end mt-4">
+                                <p class="leading-normal text-slate-800 text-lg font-semibold"><strong>Asistente:</strong>
+                                    <span class="text-slate-600 font-light my-3 capitalize">
+                                        {{ $reservation->user->name }}
+                                    </span>
+                                <p class="leading-normal text-slate-800 text-lg font-semibold"><strong>Email:</strong>
+                                    <span class="text-slate-600 font-light my-3">
+                                        {{ $reservation->user->email }}
+                                    </span>
+                                </p>
+                            @endif
+                                </p>
+                                <p class="leading-normal text-slate-800 text-lg font-semibold"><strong>Ubicación:</strong>
+                                    <span class="text-slate-600 font-light my-3 capitalize">
+                                        {{ $reservation->event->location }}
+                                    </span>
+                                </p>
+                                <p class="leading-normal text-slate-800 text-lg font-semibold"><strong>Fecha de
+                                        Inicio:</strong>
+                                    <span class="text-slate-600 font-light my-3">
+                                        {{ $reservation->event->date_start }}
+                                    </span>
+                                </p>
+                                <p class="leading-normal text-slate-800 text-lg font-semibold"><strong>Fecha de
+                                        Finalización:</strong>
+                                    <span class="text-slate-600 font-light my-3">
+                                        {{ $reservation->event->date_end }}
+                                    </span>
+                                </p>
 
-                                {{-- Form to update reservation status (cancel) --}}
-                                <form action="{{ route('reservations.update', $reservation->id) }}" method="POST"
-                                    class="update-reservation-form" data-reservation-id="{{ $reservation->id }}"
-                                    data-event-name="{{ $reservation->event->name }}">
-                                    @csrf
-                                    @method('PUT')
-                                    <input type="hidden" name="status" value="0">
+                                <p
+                                    class="{{ $reservation->status == 0 ? 'text-red-800' : 'text-green-800' }} font-semibold text-lg leading-normal my-3">
+                                    <strong>Estado:</strong> {{ $reservation->status == 0 ? 'Cancelado' : 'Activo' }}
+                                </p>
 
-                                    {{-- Show the "Cancelar Reserva" button only if the reservation is not canceled --}}
-                                    @if ($reservation->status != 0)
-                                        <button type="submit" class="bg-red-500 px-3 py-1 text-white rounded hover:bg-red-600">
-                                            Cancelar Reserva
-                                        </button>
-                                    @else
-                                        {{-- Display "Cancelado" if the reservation has been canceled --}}
-                                        <a href="#" class="bg-gray-400 px-3 py-1 text-white rounded cursor-not-allowed">
-                                            Cancelado
-                                        </a>
-                                    @endif
-                                </form>
-                            </div>
+                                <div class="flex justify-end mt-4">
+
+                                    {{-- Form to update reservation status (cancel) --}}
+                                    <form action="{{ route('reservations.update', $reservation->id) }}" method="POST"
+                                        class="update-reservation-form" data-reservation-id="{{ $reservation->id }}"
+                                        data-event-name="{{ $reservation->event->name }}">
+                                        @csrf
+                                        @method('PUT')
+                                        <input type="hidden" name="status" value="0">
+
+                                        {{-- Show the "Cancelar Reserva" button only if the reservation is not canceled --}}
+                                        @if ($reservation->status != 0)
+                                            <button type="submit"
+                                                class="bg-red-500 px-3 py-1 text-white rounded hover:bg-red-600">
+                                                Cancelar Reserva
+                                            </button>
+                                        @else
+                                            {{-- Display "Cancelado" if the reservation has been canceled --}}
+                                            <a href="#"
+                                                class="bg-gray-400 px-3 py-1 text-white rounded cursor-not-allowed">
+                                                Cancelado
+                                            </a>
+                                        @endif
+                                    </form>
+                                </div>
                         </div>
                     </div>
                 @endforeach
             </div>
         @endif
     </div>
-
 @endsection
 
 <!-- Script for SweetAlert -->
@@ -75,7 +115,8 @@
 
                 // Displays a confirmation dialog to the user
                 Swal.fire({
-                    title: "¿Estás seguro que quieres cancelar la reserva de '" + eventName + "'?",
+                    title: "¿Estás seguro que quieres cancelar la reserva de '" +
+                        eventName + "'?",
                     text: "Esta acción no se puede deshacer.",
                     icon: "warning",
                     showCancelButton: true,
@@ -90,7 +131,8 @@
                             title: "Reserva cancelada!",
                             icon: "success"
                         }).then(() => {
-                            this.submit(); // Submit the form if cancellation is confirmed
+                            this
+                                .submit(); // Submit the form if cancellation is confirmed
                         });
                     }
                 });
